08bf26a39e0670199857712d30c8d01b3837ce01
Marvin Rubio <marvinjay.rubio@ssu.edu.ph>
2024-12-07 09:59:56 +0800

    added a nav

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          prime: '#000000',
+          second: '#FFFFFF',
+          third: '#DB5151',
+        }
+      }
+    }
+  }
